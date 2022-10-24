1db7475ee4dc3b3c754d23056b4a50b69375dbcd
Raul Gomez <rgomez@assanet.com>
2022-10-24 16:26:16 -0500

    feat: configure host app with navigation

--- host/App.tsx
+++ host/App.tsx
@@ -1,103 +1,43 @@
 import { Federated } from '@callstack/repack/client';
-import React, { type PropsWithChildren } from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HostApp from './src/HostApp';
+import { Text } from 'react-native';
 
 const App1 = React.lazy(() => Federated.importModule('app1', './App1'));
 const App2 = React.lazy(() => Federated.importModule('app2', './App2'));
 
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({ children, title }) => {
-  const isDarkMode = useColorScheme() === 'dark';
+const Tab = createBottomTabNavigator();
+
+function App1Wrapper() {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+    <React.Suspense
+      fallback={<Text style={{ textAlign: 'center' }}>Loading...</Text>}>
+      <App1 />
+    </React.Suspense>
   );
-};
+}
+
+function App2Wrapper() {
+  return (
+    <React.Suspense
+      fallback={<Text style={{ textAlign: 'center' }}>Loading...</Text>}>
+      <App2 />
+    </React.Suspense>
+  );
+}
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Host App">
-            Edit <Text style={styles.highlight}>Host</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <React.Suspense fallback={<Text>Loading app1...</Text>}>
-            <App1 />
-          </React.Suspense>
-          <React.Suspense fallback={<Text>Loading app2...</Text>}>
-            <App2 />
-          </React.Suspense>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Tab.Navigator initialRouteName="Host">
+        <Tab.Screen name="Host" component={HostApp} />
+        <Tab.Screen name="App1" component={App1Wrapper} />
+        <Tab.Screen name="App2" component={App2Wrapper} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
